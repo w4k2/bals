d2315310aa75a33ffa9eef4bd746f4fd65c73071
Pawel Ksieniewicz <pawel@ksienie.com>
2020-02-23 14:23:01 +0100

    Scenario id.

--- tocsv.py
+++ tocsv.py
@@ -3,6 +3,6 @@ import config as c
 
 results = np.mean(np.squeeze(np.load("scores.npy")), axis=2)
 
-print(", ".join([i for i in c.methods().keys()]))
+print(", ".join(["scenario"] + [i for i in c.methods().keys()]))
 for i, s in enumerate(results):
-    print(", ".join(["%.3f" % val for val in results[i]]))
+    print(", ".join(["%i" % i]+["%.3f" % val for val in results[i]]))
